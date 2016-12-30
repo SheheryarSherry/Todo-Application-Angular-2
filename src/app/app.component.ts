75eeb49fe3031a1cad24a3ba4d1cd4cb5b14fe7e
Sheheryar Khan <Sheheryar Khan>
2016-12-30 09:43:14 +0500

    created service and update components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import {TodoDataService} from './todo-data.service';
+import {Todo} from './todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [TodoDataService],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+newTodo: Todo = new Todo();
+  constructor(private todoDataService: TodoDataService) {
+  }
+addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
 }
